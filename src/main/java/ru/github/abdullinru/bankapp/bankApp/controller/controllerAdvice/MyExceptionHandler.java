571275a34379d715075b34e7e094501c777d6da4
abdullinru <abdullin17021989@gmail.com>
2023-11-24 21:32:07 +0600

    добавил логгер

--- src/main/java/ru/github/abdullinru/bankapp/bankApp/controller/controllerAdvice/MyExceptionHandler.java
+++ src/main/java/ru/github/abdullinru/bankapp/bankApp/controller/controllerAdvice/MyExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.github.abdullinru.bankapp.bankApp.controller.controllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,27 +10,32 @@ import ru.github.abdullinru.bankapp.bankApp.exception.AccountNotFoundException;
 import ru.github.abdullinru.bankapp.bankApp.exception.BeneficiaryNotFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class MyExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handlerException (IllegalArgumentException e) {
+        log.error("Incorrect method parametr: {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(String.format("Error: %s", e.getMessage()));
     }
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handlerException (DataIntegrityViolationException e) {
+        log.error("Error: {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(String.format("Error: %s", e.getMessage()));
     }
     @ExceptionHandler(BeneficiaryNotFoundException.class)
     public ResponseEntity<String> handlerException (BeneficiaryNotFoundException e) {
+        log.error("Error: {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(String.format("Error: %s", e.getMessage()));
     }
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<String> handlerException (AccountNotFoundException e) {
+        log.error("Error: {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(String.format("Error: %s", e.getMessage()));
